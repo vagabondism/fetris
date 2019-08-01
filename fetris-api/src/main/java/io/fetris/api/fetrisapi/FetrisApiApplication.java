ace8f2617c836fc9157c408add8e97edc232d711
장하윤 <hyjang@woowahan.com>
2019-08-01 11:47:34 +0900

    Init Project

---
+++ fetris-api/src/main/java/io/fetris/api/fetrisapi/FetrisApiApplication.java
@@ -0,0 +1,13 @@
+package io.fetris.api.fetrisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetrisApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetrisApiApplication.class, args);
+    }
+
+}
