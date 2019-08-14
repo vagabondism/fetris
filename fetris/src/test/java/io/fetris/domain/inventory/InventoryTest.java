792bbcc228a5d4bf3b544375e267a9d6f9dd9a19
ggoomggoo <cha@hanmail2.net>
2019-08-14 11:47:56 +0900

    테스트코드내 불필요한 keepDate 값 할당 간소화

--- fetris/src/test/java/io/fetris/domain/inventory/InventoryTest.java
+++ fetris/src/test/java/io/fetris/domain/inventory/InventoryTest.java
@@ -18,7 +18,7 @@ public class InventoryTest {
         assertThat(inventory.getAllIngredients()).hasSize(0);
 
         // when
-        inventory.keep(new Ingredient("양파", LocalDate.now().minusDays(1), 5), new Ingredient("마늘", LocalDate.now().minusDays(1), 5));
+        inventory.keep(new Ingredient("양파", LocalDate.now(), 5), new Ingredient("마늘", LocalDate.now(), 5));
 
         // then
         assertThat(inventory.getAllIngredients()).hasSize(2);
@@ -27,8 +27,8 @@ public class InventoryTest {
     @Test
     public void 인밴토리는_보관된_재료를_꺼낼_수_있다() {
         // given
-        Ingredient onion = new Ingredient("양파", LocalDate.now().minusDays(1), 5);
-        Ingredient garlic = new Ingredient("마늘", LocalDate.now().minusDays(1), 5);
+        Ingredient onion = new Ingredient("양파", LocalDate.now(), 5);
+        Ingredient garlic = new Ingredient("마늘", LocalDate.now(), 5);
         Inventory inventory = new Inventory();
         inventory.keep(onion, garlic);
 
@@ -44,7 +44,7 @@ public class InventoryTest {
     public void 존재하지_않는_재료를_꺼내면_예외() {
         // given
         Inventory inventory = new Inventory();
-        Ingredient onion = new Ingredient("양파", LocalDate.now().minusDays(1), 5);
+        Ingredient onion = new Ingredient("양파", LocalDate.now(), 5);
         inventory.keep(onion);
 
         // when & than
@@ -81,38 +81,38 @@ public class InventoryTest {
     @Test
     public void 양파_5개중_3개를_꺼낸다() {
         // given
-        Ingredient oldOnion = new Ingredient("양파", LocalDate.now().minusDays(1), 5);
+        Ingredient oldOnion = new Ingredient("양파", LocalDate.now(), 5);
 
         // when
         Ingredient ingredient = new Inventory(oldOnion).takeOut("양파", 3);
 
         // then
-        assertThat(ingredient).isEqualTo(new Ingredient("양파", LocalDate.now().minusDays(1), 3));
+        assertThat(ingredient).isEqualTo(new Ingredient("양파", LocalDate.now(), 3));
     }
 
     @Test
     public void 양파_5개중_1개를_꺼낸다() {
         // given
-        Ingredient oldOnion = new Ingredient("양파", LocalDate.now().minusDays(1), 5);
+        Ingredient oldOnion = new Ingredient("양파", LocalDate.now(), 5);
 
         // when
         Inventory inventory = new Inventory(oldOnion);
         Ingredient ingredient = inventory.takeOut("양파", 1);
 
         // then
-        assertThat(ingredient).isEqualTo(new Ingredient("양파", LocalDate.now().minusDays(1), 1));
-        assertThat(inventory.takeOut("양파")).isEqualTo(new Ingredient("양파", LocalDate.now().minusDays(1), 4));
+        assertThat(ingredient).isEqualTo(new Ingredient("양파", LocalDate.now(), 1));
+        assertThat(inventory.takeOut("양파")).isEqualTo(new Ingredient("양파", LocalDate.now(), 4));
     }
 
     @Test
     public void 양파_3개중_3개를_꺼낸다() {
         // given
-        Ingredient oldOnion = new Ingredient("양파", LocalDate.now().minusDays(1), 3);
+        Ingredient oldOnion = new Ingredient("양파", LocalDate.now(), 3);
 
         // when
         Ingredient ingredient = new Inventory(oldOnion).takeOut("양파", 3);
 
         // then
-        assertThat(ingredient).isEqualTo(new Ingredient("양파", LocalDate.now().minusDays(1), 3));
+        assertThat(ingredient).isEqualTo(new Ingredient("양파", LocalDate.now(), 3));
     }
 }
